4bd0186f2c7a97c3d5b97ab4c26fa1fcb0d9117a
Earbunsong <bunsong601@gmail.com>
2023-04-18 15:50:10 +0700

    thymeleaf-webapp

---
+++ src/main/java/com/example/thymeleaf/webapp/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.thymeleaf.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    String viewHome(){
+        return "pages/index";
+    }
+}
---
+++ src/test/java/com/example/thymeleaf/webapp/ThymeleafWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
